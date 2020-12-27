952957abb2dc0d9011a6934a0bef72cdd691e72f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-12-26 23:10:24 -0500

    don't transpile with babel
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- .babelrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-    presets: ["@babel/preset-env"],
-    minified: false,
-    shouldPrintComment: (val) => (
-        (/@/u).test(val) && !((/eslint|istanbul/u).test(val))
-    ),
-    comments: true,
-}
--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@
  * @author Luke Zhang https://luke-zhang-04.github.io
  *
  * @copyright 2018 - 2019 satya164, 2020 Luke Zhang
- * @version 1.0.3
+ * @version 1.0.4
  */
 
 /**
