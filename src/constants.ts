4981e124a2b3478766f7ad3ea2ed6e7e296ea03e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-12-26 22:46:28 -0500

    version to 1.0.3
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
  * @author Luke Zhang https://luke-zhang-04.github.io
  *
  * @copyright 2018 - 2019 satya164, 2020 Luke Zhang
- * @version 1.0.2
+ * @version 1.0.3
  *
  * @file constant values used by the main component
  */
